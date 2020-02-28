c3cbe44828647a49854b5b41f1b816a5fe958651
addin <akhul.syaifudin@rumahweb.co.id>
2020-02-28 10:38:03 +0700

    re arrange ui , dummy api

---
+++ ext.js
@@ -0,0 +1,21 @@
+var arr  = []
+
+var ext_rekomended = [
+  { "id" : "1", "ext" : ".com"  },
+  { "id" : "2", "ext" : ".club" },
+  { "id" : "3", "ext" : ".shop" },
+  { "id" : "4", "ext" : ".xyz"  },
+  { "id" : "5", "ext" : ".net"  },
+  { "id" : "6", "ext" : ".org"  }
+]
+
+var ext_indonesia = [
+  {"id" : "1","ext" : ".id"     },
+  {"id" : "2","ext" : ".my.id"  },
+  {"id" : "3","ext" : ".sch.id" },
+  {"id" : "4","ext" : "go.id"   },
+  {"id" : "5","ext" : "or.id"   },
+  {"id" : "6","ext" : "web.id"  }
+]
+
+//arr.push([rekomended, indonesia])
--- main.js
+++ main.js
@@ -26,101 +26,94 @@ Vue.component("x-newdomain",{
     template : `
     <div class="jumbotronr" style="margin-top:3%;border-radius:0px">
         <div class="row">
-            <div class="col-sm-7">
+            <div class="col-sm-2">
+                <ul>
+                    <li>Promo</li>
+                    <li>Popular</li>
+                    <li>Indonesia</li>
+                </ul>
+            </div>
+            <div class="col-sm-10">
                 <form class="form-horizontal" action="/action_page.php">
                     <div class="form-group">
                         <div class="col-sm-12">
-                            <input type="text" v-model="domains" class="form-control input-lg" placeholder="Enter domain">
+                            <input type="text" v-model="domain" class="form-control input-lg" placeholder="Enter domain">
                         </div>
                     </div>
                 </form>
 
                 <div class="row">
-                    <div class="col-sm-4">
-                        <p>ext your choice is :</p>
-                        <ul>
-                            <li v-for="row in choices" >{{ row }}</li>
-                        </ul>
+                    <div class="col-sm-6">
+                        <div class="panel panel-default panel-tld">
+                            <div class="panel-heading"><strong>Ekstensi Domain yang Tersedia</strong></div>
+                            <div class="panel-body">
+                                <ul class="list-unstyled list-tld row">
+                                    <li class="col-sm-3 col-xs-6 list-tld" v-for="ext in exts" :key="ext.id">
+                                        <label class="btn btn-default btn-sm">
+                                        <input type="checkbox" :value="ext.ext" v-model="choices">  
+                                        {{ ext.ext }} 
+                                        </label>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-sm-8">
-                        <p>tes req</p>
+                    <div class="col-sm-6">
+                        <div class="panel panel-default">
+                            <div class="panel-heading"><b>Result {{ domain }}</b></div>
+                            <div class="panel-body">
+                                <div v-if="choices.length === 0">
+                                    <p><i>empty</i></p>
+                                </div>
+                                <div v-else>
+                                    <div v-if="sttsReq.length === 0" class="text-center">
+                                        <i class="fa fa-spin fa-spinner fa-4x"></i>
+                                        <p>Please wait ...</p>
+                                    </div>
+                                    <div>
+                                        <ul>
+                                            <li v-for="rs in requestAPI"><b>{{rs.domain}}</b> {{ rs.stts }}</li>
+                                        </ul>
+                                    </div>
 
-                        <div v-if="sttsReq.length === 0">
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-striped active" role="progressbar"
-                                aria-valuenow="99" aria-valuemin="0" aria-valuemax="100" style="width:99%">
-                                99%
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <ul>
-                                <li v-for="rs in requestAPI">{{rs.domain}} {{ rs.stts }}</li>
-                            </ul>
-                        </div>
-                        
                     </div>
+                    
                 </div>
             </div>
             <div class="col-sm-5">
-                <div class="panel panel-default panel-tld">
-                    <div class="panel-heading"><strong>Ekstensi Domain yang Tersedia</strong></div>
-                    <div class="panel-body">
-                        <ul class="list-unstyled list-tld row">
-                            <li class="col-sm-3 col-xs-6" v-for="ext in exts" :key="ext.id">
-                                <label class="btn btn-default btn-sm">
-                                <input type="checkbox" :value="ext.extD" v-model="choices">  
-                                {{ ext.extD }} 
-                                </label>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                
             </div>
         </div>        
     </div>
     `,
     data() {
         return {
-            exts : [
-                {
-                    id : 1,
-                    extD : ".com"
-                },
-                {
-                    id : 2,
-                    extD : ".net"
-                },
-                {
-                    id : 3,
-                    extD : ".id"
-                },
-                {
-                    id : 4,
-                    extD : ".xyz"
-                },
-
-            ],
+            exts : ext_rekomended,
             choices : [],
-            domains : "akhulsyaifudin",
+            domain : "akhulsyaifudin",
             sttsReq : []
         }
     },
-    methods: {
-        ajax(ext){
-            console.log(ext+" ajax request ...")
-        },
-    },
     computed: {
         requestAPI() {
-            
-            let dom = this.domains
+            // let URLAPI = "https://order2.rumahweb.com/order/siapa/domain/";
+            let URLAPI = "http://ron-swanson-quotes.herokuapp.com/v2/quotes/";
+
+            let dom = this.domain
             let stts = []
             let choices = this.choices
 
             for (let i = 0; i < choices.length; i++){
+
                 let x = dom+choices[i]
-                axios.get("https://order2.rumahweb.com/order/siapa/domain/"+x)
+
+                // logs
+                console.log("request API : ".URLAPI+x)
+
+                axios.get(URLAPI+x)
                 .then(function(res){
                     stts.push(
                         {
@@ -130,7 +123,6 @@ Vue.component("x-newdomain",{
                     )
                 })
             }
-            // console.log(stts)
             return this.sttsReq = stts
             
         }
@@ -190,7 +182,7 @@ Vue.component("x-tabs",{
 
 var componentLocal = {
     template :`
-        <h1>componen local gaes</h1>
+       
     ` 
 }
 
